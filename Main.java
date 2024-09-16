7b8d95e2539cf00d5082b362daa1558ed256fd12
Samim29 <samimalisk000@gmail.com>
2024-09-16 12:13:59 +0530

    it is first java programm

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args){
+        // Our first Programme
+        System.out.println("Hello World");
+
+        // Variable
+        String name="Aman";
+        String neighbour= "Akku";
+        
+
+        //primitive Data type 
+        byte age= 30;
+        int phone=1234567890;
+        long phone2=12345678900L;
+        float pi=3.14F;
+        char letter='@';
+        boolean isAdult= true;
+
+        //Non-primitive Data type 
+        String name2 ="Samim";
+        String brother= new String("Sadik");
+        System.out.println(name2.length());
+
+    }
+}
